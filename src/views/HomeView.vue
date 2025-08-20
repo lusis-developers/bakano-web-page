ca0c7b3a5625b21590a71dc900ef466421dc8ae9
Diego Reyes <diegorele13@gmail.com>
2025-08-19 22:42:55 -0500

    feat(HomeView): add TheTeam component to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import TheHero from '@/components/TheHero.vue'
 import TheStrategies from '@/components/TheStrategies.vue'
 import TheTestimonials from '@/components/TheTestimonials.vue'
+import TheTeam from '@/components/TheTeam.vue'
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import TheTestimonials from '@/components/TheTestimonials.vue'
     <TheHero />
     <TheStrategies />
     <TheTestimonials />
+    <TheTeam />
     
     <!-- Aquí se pueden agregar más secciones de la página principal -->
     <!-- <AboutSection /> -->
